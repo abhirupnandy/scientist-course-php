ab96cca2c2c1d0ff1464e10dfe36596e288350df
Abhirup Nandy <abhirupnandy.rocks@gmail.com>
2023-11-02 02:31:13 +0530

    EP18 - Database file create

--- index.php
+++ index.php
@@ -40,8 +40,8 @@
 	
 	$db = new Database();
 	$posts = $db -> query("select * from posts where id > 1") -> fetch(PDO::FETCH_ASSOC);
-
-//	foreach ( $posts as $post ) {
-//		echo "<li>" . $post['title'] . "</li>";
-//	}
-	dd($posts);
+	
+	//	foreach ( $posts as $post ) {
+	//		echo "<li>" . $post['title'] . "</li>";
+	//	}
+	//	dd($posts);
